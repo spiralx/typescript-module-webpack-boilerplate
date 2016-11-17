fbae411df213ad0c936aa7cebc2d01fe33700d00
James Skinner <spiralx@gmail.com>
2016-11-17 13:48:04 +0000

    Basic test skeleton

---
+++ test/helper.js
@@ -0,0 +1,3 @@
+// Add expect to globals and so we can use it after
+// using '--require chai' to auto-load chai.
+global.expect = require('chai').expect
---
+++ test/index.spec.ts
@@ -0,0 +1,11 @@
+import lib from '../src'
+
+// ----------------------------------------------------------------------------
+
+describe('module_name', () => {
+
+  it('should do a thing', () => {
+    expect(lib).to.exist
+  })
+
+})
